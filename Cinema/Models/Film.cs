2e22a7233b2fd6174f2b4b8d64648ea589294956
CsharpMaster37 <misha310303@gmail.com>
2024-04-12 00:45:04 +0300

    Подгрузил необходимые пакеты, начал создание моделей

---
+++ Cinema/Models/Film.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.Models
+{
+    public class Film
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста введите название")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Длина строки должна быть от 1 до 50 символов")]
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста введите страну выпуска")]
+        [StringLength(30, MinimumLength = 1, ErrorMessage = "Длина строки должна быть от 1 до 30 символов")]
+        [Display(Name = "Страна")]
+        public string Country { get; set; }      
+
+        [Required]
+        [Range(0, 2000, ErrorMessage = "Недопустимая цена")]
+        [Display(Name = "Цена")]
+
+        public int Price { get; set; }
+        [Required]
+        [Range(1895, 2024, ErrorMessage = "Недопустимый год")]
+        [Display(Name = "Год выпуска")]
+        public int Year { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public int? GenreID { get; set; }
+
+        [Display(Name = "Жанр")]
+        public Genre Genre { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public string? ImageUrl { get; set; }
+    }
+}
---
+++ Cinema/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.Models
+{
+    public class Genre
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста введите название жанра")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 20 символов")]
+        [Display(Name = "Жанр")]
+        public string Name { get; set; } // название 
+        public IEnumerable<Film> Films { get; set; }
+    }
+}
